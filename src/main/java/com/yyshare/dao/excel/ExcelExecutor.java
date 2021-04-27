b3fa798ac72103aa5f17555dfa1dda4b7c75ef03
yuanye <yuanye@iboxpay.com>
2021-04-27 16:35:45 +0800

    excel存储组件执行器

--- src/main/java/com/yyshare/dao/excel/ExcelExecutor.java
+++ src/main/java/com/yyshare/dao/excel/ExcelExecutor.java
@@ -6,13 +6,17 @@ import com.yyshare.util.ClassUtil;
 import lombok.extern.slf4j.Slf4j;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * excel存储组件执行器
  */
 @Slf4j
 public class ExcelExecutor implements IExecutor {
+
+    private Map<String, Object> impls = new HashMap<>();
 
     {
         try {
@@ -22,17 +26,36 @@ public class ExcelExecutor implements IExecutor {
                 if (c == ExcelExecutor.class) {
                     continue;
                 }
-
+                Object obj = c.getConstructor().newInstance();
+                Class[] infs = c.getInterfaces();
+                for (Class inf : infs) {
+                    impls.put(inf.getName(), obj);
+                }
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            log.warn("excel存储组件初始化异常", e);
             throw new ShareException("excel存储组件初始化异常");
         }
     }
 
     @Override
     public Object invoke(Method method, Object[] args) {
-        log.info("excel executor");
-        return null;
+        Object obj = getInvokeObj(method);
+        if (obj == null) {
+            log.info("excel存储执行器不支持此方法");
+            return null;
+        }
+        try {
+            return method.invoke(obj, args);
+        } catch (ShareException e) {
+            throw e;
+        } catch (Exception e) {
+            log.error("excel存储执行器出现未知异常", e);
+            throw new ShareException("excel存储执行器出现未知异常");
+        }
+    }
+
+    private Object getInvokeObj(Method method) {
+        return impls.get(method.getDeclaringClass().getName());
     }
 }
